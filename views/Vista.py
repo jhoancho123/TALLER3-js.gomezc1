02bb6e67b0dd0a6be5d061415c7a8c3d329427b5
JOHAN SEBASTIAN GOMEZ CHAVISTA <gomezjos@HILDEBOG.LOCAL>
2024-11-24 20:17:35 -0500

    taller

--- views/Vista.py
+++ views/Vista.py
@@ -6,7 +6,3 @@ class Vista:
     def mostrar_flete(self, flete: float):
         """Muestra el costo de importación del animal"""
         print(f"El costo de importación es: ${flete:.2f}")
-
-    def mostrar_ratones_comidos(self, ratones_comidos: int):
-        """Muestra cuántos ratones ha comido la boa"""
-        print(f"La boa ha comido {ratones_comidos} ratones.")
